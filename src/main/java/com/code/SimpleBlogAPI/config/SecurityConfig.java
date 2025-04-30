5eaa8d078c97635e26565eef8ff057a52379c46a
r9988.2588 <r9988.2588@gmail.com>
2025-04-30 08:24:25 +0000

    created the in memory users

--- src/main/java/com/code/SimpleBlogAPI/config/SecurityConfig.java
+++ src/main/java/com/code/SimpleBlogAPI/config/SecurityConfig.java
@@ -2,10 +2,23 @@ package com.code.SimpleBlogAPI.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -16,7 +29,12 @@ public class SecurityConfig {
         http
 
                 .authorizeHttpRequests(auth -> auth
+                        // allowing the people to view posts without any authentication
                         .requestMatchers("/blog/posts", "blog/posts/{id}").permitAll()
+
+                        // allowed to register the user
+                        .requestMatchers("/blog/register").permitAll()
+
                         // Allowing H2 console to access
                         .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated())
@@ -27,4 +45,57 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService,
+            BCryptPasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        return new ProviderManager(authenticationProvider);
+    }
+
+    @Bean
+    DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    @Bean
+    public JdbcUserDetailsManager jdbcUserDetailsManager() {
+        return new JdbcUserDetailsManager(dataSource());
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(JdbcUserDetailsManager manager) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        // if (!manager.userExists("user")) {
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("adpass"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("pass"))
+                .roles("USER")
+                .build();
+        manager.createUser(admin);
+        manager.createUser(user);
+        // }
+        return manager;
+    }
+
+    // created the password encoder
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
